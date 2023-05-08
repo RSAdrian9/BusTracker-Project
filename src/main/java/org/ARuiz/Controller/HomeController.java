d06e7fc7e61e5769a8fa64aa8005f2b6cd1f13e5
adrian <adrian4rs@gmail.com>
2023-05-08 11:18:23 +0200

    Update View & Controller Home

--- src/main/java/org/ARuiz/Controller/HomeController.java
+++ src/main/java/org/ARuiz/Controller/HomeController.java
@@ -1,13 +1,5 @@
 package org.ARuiz.Controller;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
-import org.ARuiz.App;
-
 public class HomeController {
 
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("login");
-    }
 }
