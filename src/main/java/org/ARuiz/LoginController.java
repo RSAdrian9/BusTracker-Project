870fa5cbf783a73a14d259238cce96c4318886fe
adrian <adrian4rs@gmail.com>
2023-05-25 00:17:20 +0200

    v2.3.3 - Login Controller and View

--- src/main/java/org/ARuiz/LoginController.java
+++ src/main/java/org/ARuiz/LoginController.java
@@ -2,6 +2,9 @@ package org.ARuiz;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
@@ -11,6 +14,7 @@ import org.ARuiz.App;
 import org.ARuiz.Model.Connections.ConnectionMySQL;
 import org.ARuiz.Model.DAO.AdminDAO;
 import org.ARuiz.Model.Domain.Admin;
+import org.ARuiz.Util.Utils;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -38,12 +42,23 @@ public class LoginController {
         if (!txtfld_username.getText().isEmpty() && !txtfld_password.getText().isEmpty()) {
             String user = txtfld_username.getText();
             String password = txtfld_password.getText();
-            AdminDAO aDAO = new AdminDAO();
-            boolean state = aDAO.getAdminByUsernameAndPassword(user, password);
-            if (state) {
+
+            // Validar el nombre de usuario y la contraseña utilizando los métodos de la clase Utils
+            boolean validUsername = Utils.validateUsername(user);
+            boolean validPassword = Utils.validatePassword(password);
+
+            if (validUsername && validPassword) {
+                // Las credenciales son válidas, realizar acciones adicionales
+                // Ejemplo: Acceder a la página principal de la aplicación
                 App.setRoot("HomeView");
+            } else {
+                // Las credenciales no son válidas, mostrar un mensaje de error o realizar otra acción
+                // Ejemplo: Mostrar un mensaje de error indicando que las credenciales son incorrectas
+                System.out.println("Credenciales incorrectas");
             }
         }
+    }
+
 
         /*String user = txtfld_username.getText();
         String password = txtfld_password.getText();
@@ -57,20 +72,38 @@ public class LoginController {
         } else {
             showAlert("Inicio de sesión fallido", "Usuario o contraseña incorrectos");
         } */
-    }
+
 
 
     @FXML
     private void btnSignupValidate(ActionEvent event) {
-        String user = txtfld_username.getText();
-        String password = txtfld_password.getText();
+        try {
+            // Cargar la vista LineViewAdmin desde su archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("SignUpView.fxml"));
+            Parent signUpView = loader.load();
 
-        if (createUser(user, password)) {
+            // Obtener el Stage actual
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Crear una nueva escena con la vista LineViewAdmin
+            Scene scene = new Scene(signUpView);
+
+            // Establecer la nueva escena en el Stage
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Manejar cualquier excepción que pueda ocurrir durante la carga de la vista
+        }
+    }
+
+    /*
+            if (createUser(user, password)) {
             showAlert("Cuenta creada", "¡La cuenta se ha creado exitosamente!");
         } else {
             showAlert("Error al crear cuenta", "No se pudo crear la cuenta. Inténtalo de nuevo.");
         }
-    }
+     */
 
 
     private boolean authenticateUser(String user, String password) {
