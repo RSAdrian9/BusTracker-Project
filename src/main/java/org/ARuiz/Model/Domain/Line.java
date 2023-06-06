aebfa6d456f05efb8ada102746e77914e515df04
adrian <adrian4rs@gmail.com>
2023-06-06 20:55:04 +0200

    v2.5.1 - Update Line Class, Controller, DAO

--- src/main/java/org/ARuiz/Model/Domain/Line.java
+++ src/main/java/org/ARuiz/Model/Domain/Line.java
@@ -1,25 +1,22 @@
 package org.ARuiz.Model.Domain;
 
-import java.time.LocalTime;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Line {
     private int id_bus;
-    private String line_name;
+    private String name;
     private int place;
-    private String route;
-    private LocalTime timetable;
+    private List<Stop> stops;
 
-    public Line(int id_bus, String line_name, int place, String route, LocalTime timetable) {
+    public Line(int id_bus, String name, int place) {
         this.id_bus = id_bus;
-        this.line_name = line_name;
+        this.name = name;
         this.place = place;
-        this.route = route;
-        this.timetable = timetable;
     }
 
     public Line() {
-
+        stops = new ArrayList<>();
     }
 
     public int getId_bus() {
@@ -30,78 +27,37 @@ public class Line {
         this.id_bus = id_bus;
     }
 
-    public String getLine_name() {
-
-        return line_name;
+    public String getName() {
+        return name;
     }
 
-    public void setLine_name(String line_name) {
-
-        this.line_name = line_name;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getPlace() {
-
         return place;
     }
 
     public void setPlace(int place) {
-
         this.place = place;
     }
 
-    public String getRoute() {
-
-        return route;
+    public List<Stop> getStops() {
+        return stops;
     }
 
-    public void setRoute(String route) {
-
-        this.route = route;
-    }
-
-    public LocalTime getTimetable() {
-
-        return timetable;
-    }
-
-    public void setTimetable(LocalTime timetable) {
-
-        this.timetable = timetable;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Line line = (Line) o;
-
-        if (id_bus != line.id_bus) return false;
-        if (place != line.place) return false;
-        if (!Objects.equals(line_name, line.line_name)) return false;
-        if (!Objects.equals(route, line.route)) return false;
-        return Objects.equals(timetable, line.timetable);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id_bus;
-        result = 31 * result + (line_name != null ? line_name.hashCode() : 0);
-        result = 31 * result + place;
-        result = 31 * result + (route != null ? route.hashCode() : 0);
-        result = 31 * result + (timetable != null ? timetable.hashCode() : 0);
-        return result;
+    public void setStops(List<Stop> stops) {
+        this.stops = stops;
     }
 
     @Override
     public String toString() {
         return "Line{" +
                 "id_bus=" + id_bus +
-                ", line_name='" + line_name + '\'' +
+                ", name='" + name + '\'' +
                 ", place=" + place +
-                ", route='" + route + '\'' +
-                ", timetable=" + timetable +
+                ", stops=" + stops +
                 '}';
     }
 }
